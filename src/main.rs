7a8cc75e1e01b31969945f0852aef998241110b1
Sanosuke Kato <plus.wisteria38@gmail.com>
2019-09-25 14:02:07 +0900

    Replace to read_exact/write_all instead to read/write.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?;
+    stream.read_exact(&mut buffer)?;
     let s = match str::from_utf8(&buffer) {
         Ok(v) => v,
         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
     };
     println!("{}", s);
 
-    stream.write(b"HTTP/1.1 200 OK\r\n")?;
-    stream.write(b"\r\n")?;
-    stream.write(b"Hello World!")?;
+    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+    stream.write_all(b"\r\n")?;
+    stream.write_all(b"Hello World!")?;
     stream.flush()?;
 
     Ok(())
